4c8d342463dbf2b3429633cd5fe5042d14d37554
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-10-19 19:38:55 +0100

    intial push

--- resources/main.js
+++ resources/main.js
@@ -1,14 +1,19 @@
-function setBars(dataArr, bars) { 
+function setData(dataArr, bars, spend) { 
     const sorted = [...dataArr.sort((a, b)=> b.amount - a.amount)]
     const hundredP = sorted[0].amount;
     sorted.forEach(day => {
     day.percent = String(Math.round((day.amount / hundredP) * 100));
+    day.spendID = "spend" + day.day;
     })
 
     for(let i = 0; i < bars.length; i++) {
         for(let j = 0; j < sorted.length; j++) {
+
             if(bars[i].id === sorted[j].day) {
                 bars[i].style.height = sorted[j].percent+'%'; 
+            if(spend[i].id === sorted[j].spendID) {
+                spend[i].innerHTML = "£" + sorted[j].amount;
+            }
                 continue
             }
         }
@@ -18,6 +23,19 @@ function setBars(dataArr, bars) {
     let day = d.getDay();
 
     bars[day].classList.add('today')
+}
+
+function barHover(bars, spend){
+    for(let i = 0; i < bars.length; i++) {
+        bars[i].addEventListener("mouseover", () => {
+            spend[i].classList.add('active')
+            spend[i].classList.remove('hidden') 
+        })
+        bars[i].addEventListener("mouseout", () => {
+            spend[i].classList.remove('active')
+            spend[i].classList.add('hidden') 
+        })
+    }
 }
 
 
@@ -31,8 +49,9 @@ fetch('./data.json')
     const bars = [...document.querySelectorAll('.bar')]
     const spend = [...document.querySelectorAll('.spend')]
 
-    setBars(json, bars)
 
+    setData(json, bars, spend)
+    barHover(bars, spend)
     
 
 });
